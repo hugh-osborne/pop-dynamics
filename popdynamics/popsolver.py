83a8dd7f7f13188015ff99b5ae675f4b4c698cca
hugh-osborne <hugh.osborne@gmail.com>
2023-07-28 16:57:48 +0100

    CPU version set up with conductance neuron population test.

---
+++ popdynamics/popsolver.py
@@ -0,0 +1,157 @@
+import numpy as np
+
+class Solver:
+    def __init__(self, _func, initial_distribution, _base, _cell_widths, _mass_epsilon):
+        self.dims = _base.shape[0]
+        self.cell_widths = _cell_widths
+
+        self.cell_buffers = [{},{}]
+
+        self.cell_base = np.zeros(self.dims)
+        self.current_buffer = 0
+        self.remove_sum = 1.0
+
+        self.noise_kernels = []
+
+        self.mass_epsilon = _mass_epsilon
+        self.func = _func
+
+        first_cell = True
+        cell_base_coords = np.zeros(self.dims)
+        for idx, val in np.ndenumerate(initial_distribution):
+            if val > 0.0:
+                if first_cell:
+                    cell_base_coords = idx
+                    self.cell_base = _base + (np.multiply(idx,_cell_widths))
+                    first_cell = False
+                self.cell_buffers[self.current_buffer][tuple((np.asarray(idx)-cell_base_coords).tolist())] = [val, []]
+
+        # init first cell buffer
+
+        for coord in self.cell_buffers[self.current_buffer]:
+            centroid = [0 for a in range(self.dims)]
+
+            for d in range(self.dims):
+                centroid[d] = self.cell_base[d] + ((coord[d]+0.5)*self.cell_widths[d])
+
+            stepped_centroid = self.func(centroid)
+
+            self.cell_buffers[self.current_buffer][coord][1] = self.calcTransitions(centroid, stepped_centroid, coord)
+
+    def addNoiseKernel(self, kernel, dimension):
+        kernel_transitions = {}
+        cs = tuple(np.zeros(self.dims).tolist())
+        kernel_transitions[cs] = []
+        for c in range(len(kernel)):
+            if kernel[c] > 0.0:
+                kernel_transitions[cs] = kernel_transitions[cs] + [(kernel[c], [c-int(len(kernel)/2) if d == dimension else 0 for d in range(self.dims)])]
+        self.noise_kernels = self.noise_kernels + [[1.0,kernel_transitions[cs]]]
+
+    def calcTransitions(self, centroid, stepped_centroid, coord, d=0, target_coord=[], mass=1.0):
+        if len(target_coord) == len(coord):
+            return [(mass, target_coord)]
+
+        diff = stepped_centroid[d] - centroid[d]
+        cell_lo = coord[d] + int(diff / self.cell_widths[d])
+        cell_hi = cell_lo + 1
+        prop_lo = 0.0
+        if diff < 0.0: # actually, diff is negative so cell_lo is the upper cell
+            cell_hi = cell_lo - 1
+            prop_lo = ((diff % self.cell_widths[d]) / self.cell_widths[d])
+        else:
+            prop_lo = 1.0 - ((diff % self.cell_widths[d]) / self.cell_widths[d])
+        prop_hi = 1.0 - prop_lo
+    
+        return self.calcTransitions(centroid, stepped_centroid, coord, d+1, target_coord + [cell_lo], mass*prop_lo) + self.calcTransitions(centroid, stepped_centroid, coord, d+1, target_coord + [cell_hi], mass*prop_hi)
+
+    def updateCell(self, relative, new_cell_dict, transition, coord, mass, func):
+        t = [a for a in transition[1]]
+        if relative:
+            for d in range(len(coord)):
+                t[d] = coord[d] + t[d]
+
+        if tuple(t) not in new_cell_dict.keys():
+            new_cell_dict[tuple(t)] = [0.0,[]]
+            centroid = [0 for a in range(len(coord))]
+
+            for d in range(len(coord)):
+                centroid[d] = self.cell_base[d] + ((t[d]+0.5)*self.cell_widths[d])
+
+            stepped_centroid = func(centroid)
+            new_cell_dict[tuple(t)][1] = self.calcTransitions(centroid, stepped_centroid, t)
+    
+        new_cell_dict[tuple(t)][0] += mass*transition[0]
+
+    def calcCellCentroid(self, coords):
+        centroid = [0 for a in range(self.dims)]
+
+        for d in range(self.dims):
+            centroid[d] = self.cell_base[d] + ((coords[d]+0.5)*self.cell_widths[d])
+
+        return centroid
+
+    def calcMarginals(self):
+        vs = [{} for d in range(self.dims)]
+        for c in self.cell_buffers[self.current_buffer]:
+            for d in range(self.dims):
+                if c[d] not in vs[d]:
+                    vs[d][c[d]] = self.cell_buffers[self.current_buffer][c][0]
+                else:
+                    vs[d][c[d]] += self.cell_buffers[self.current_buffer][c][0]
+
+        final_vs = [[] for d in range(self.dims)]
+        final_vals = [[] for d in range(self.dims)]
+
+        for d in range(self.dims):
+            for v in vs[d]:
+                final_vs[d] = final_vs[d] + [self.cell_base[d] + (self.cell_widths[d]*(v))]
+                final_vals[d] = final_vals[d] + [vs[d][v]]
+
+        return final_vs, final_vals
+
+    def updateDeterministic(self):
+        # Set the next buffer mass values to 0
+        for a in self.cell_buffers[(self.current_buffer+1)%2].keys():
+            self.cell_buffers[(self.current_buffer+1)%2][a][0] = 0.0
+    
+        # Fill the next buffer with the updated mass
+        for coord in self.cell_buffers[self.current_buffer]:
+            self.cell_buffers[self.current_buffer][coord][0] /= self.remove_sum
+            for ts in self.cell_buffers[self.current_buffer][coord][1]:
+                self.updateCell(False, self.cell_buffers[(self.current_buffer+1)%2], ts, coord, self.cell_buffers[self.current_buffer][coord][0], self.func)
+
+        # Remove any cells with a small amount of mass and keep a total to spread back to the remaining population
+        remove = []
+        self.remove_sum = 1.0
+        for a in self.cell_buffers[(self.current_buffer+1)%2].keys():
+            if self.cell_buffers[(self.current_buffer+1)%2][a][0] < self.mass_epsilon:
+                remove = remove + [a]
+                self.remove_sum -= self.cell_buffers[(self.current_buffer+1)%2][a][0]
+
+        for a in remove:
+            self.cell_buffers[(self.current_buffer+1)%2].pop(a, None)
+
+        # swap the buffer counter
+        self.current_buffer = (self.current_buffer+1)%2
+
+    def applyNoiseKernels(self):
+        for kernel in self.noise_kernels:
+            for a in self.cell_buffers[(self.current_buffer+1)%2].keys():
+                self.cell_buffers[(self.current_buffer+1)%2][a][0] = 0.0
+    
+            for coord in self.cell_buffers[self.current_buffer]:
+                self.cell_buffers[self.current_buffer][coord][0] /= self.remove_sum
+                for ts in kernel[1]:
+                    self.updateCell(True, self.cell_buffers[(self.current_buffer+1)%2], ts, coord, self.cell_buffers[self.current_buffer][coord][0], self.func)
+
+            remove = []
+            self.remove_sum = 1.0
+            for a in self.cell_buffers[(self.current_buffer+1)%2].keys():
+                if self.cell_buffers[(self.current_buffer+1)%2][a][0] < self.mass_epsilon:
+                    remove = remove + [a]
+                    self.remove_sum -= self.cell_buffers[(self.current_buffer+1)%2][a][0]
+
+            for a in remove:
+                self.cell_buffers[(self.current_buffer+1)%2].pop(a, None)
+
+            self.current_buffer = (self.current_buffer+1)%2
---
+++ tests/neuron_population.py
@@ -0,0 +1,185 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from scipy.stats import poisson
+
+from popdynamics.popsolver import Solver
+
+def cond(y):
+    E_l = -70.6
+    E_e = 0.0
+    E_i = -75
+    C = 281
+    g_l = 0.03
+    tau_e =2.728
+    tau_i = 10.49
+
+    threshold = -50.4
+    reset = -70.6
+
+    v = y[0]
+    w = y[1]
+    u = y[2]
+
+    v_prime = (-g_l*(v - E_l) - w * (v - E_e) - u * (v - E_i)) / C
+    w_prime = -(w) / tau_e
+    u_prime = -(u) / tau_i
+
+    return [v + 0.1*v_prime, w + 0.1*w_prime, u + 0.1*u_prime]
+
+v_res = 100
+w_res = 100
+u_res = 100
+I_res = 300
+
+v_max = -40.0
+v_min = -80.0
+w_max = 50.0
+w_min = -5.0
+u_max = 50.0
+u_min = -5.0
+
+# Set up the starting distribution
+v = np.linspace(v_min, v_max, v_res)
+w = np.linspace(w_min, w_max, w_res)
+u = np.linspace(u_min, u_max, u_res)
+
+#[-70.6, 0.001, 0.001]
+# Unfortunately stats.norm doesn't provide a nice pmf approximation of the pdf. 
+# So let's just do that ourselves without breaking our backs by multiplying across by the discretisation width and normalising
+vpdf = [a * ((v_max-v_min)/v_res) for a in norm.pdf(v, -70.6, 0.1)]
+wpdf = [a * ((w_max-w_min)/w_res) for a in norm.pdf(w, 0.0, 0.1)]
+updf = [a * ((u_max-u_min)/u_res) for a in norm.pdf(u, 0.0, 0.1)]
+
+vpdf = [a / sum(vpdf) for a in vpdf]
+wpdf = [a / sum(wpdf) for a in wpdf]
+updf = [a / sum(updf) for a in updf]
+
+# Poisson inputs
+
+w_rate = 4
+epsp = 0.5
+wI_max_events = 5
+wI_min_events = -5
+wI_max = wI_max_events*epsp
+wI_min = wI_min_events*epsp
+epsps = np.linspace(wI_min, wI_max, I_res)
+wI_events = np.linspace(wI_min_events, wI_max_events, I_res)
+wIpdf_final = [0 for a in wI_events]
+for i in range(len(wI_events)-1):
+    if (int(wI_events[i]) < int(wI_events[i+1])) or (wI_events[i] < 0 and wI_events[i+1] >= 0): # we have just passed a new event
+        e = int(wI_events[i+1])
+        if e <= 0:
+            e = int(wI_events[i])
+        diff = wI_events[i+1] - wI_events[i]
+        lower_prop = (int(wI_events[i+1]) - wI_events[i]) / diff 
+        upper_prop = 1.0 - lower_prop
+        wIpdf_final[i] += poisson.pmf(e, w_rate*0.1) * lower_prop
+        wIpdf_final[i+1] += poisson.pmf(e, w_rate*0.1) * upper_prop
+wIpdf = wIpdf_final
+
+
+wI_res = int((wI_max-wI_min) / ((w_max-w_min)/w_res))+1
+pymiind_wI = [0 for a in range(wI_res)]
+ratio = I_res / wI_res
+val_counter = 0.0
+pos_counter = 0
+for i in range(I_res):
+    pos_counter += 1
+    if pos_counter > ratio:
+        pos_counter = 0
+        val_counter += wIpdf[i] * (i % ratio)
+        pymiind_wI[int(i/ratio)] = val_counter
+        val_counter = wIpdf[i] * (1.0-(i % ratio))
+    else:
+        val_counter += wIpdf[i]
+
+u_rate = 2
+ipsp = 0.5
+uI_max_events = 5
+uI_min_events = -5
+uI_max = wI_max_events*ipsp
+uI_min = wI_min_events*ipsp
+ipsps = np.linspace(uI_min, uI_max, I_res)
+uI_events = np.linspace(uI_min_events, uI_max_events, I_res)
+uIpdf_final = [0 for a in uI_events]
+for i in range(len(uI_events)-1):
+    if (int(uI_events[i]) < int(uI_events[i+1])) or (uI_events[i] < 0 and uI_events[i+1] >= 0): # we have just passed a new event
+        e = int(uI_events[i+1])
+        if e <= 0:
+            e = int(uI_events[i])
+        diff = uI_events[i+1] - uI_events[i]
+        lower_prop = (int(uI_events[i+1]) - uI_events[i]) / diff 
+        upper_prop = 1.0 - lower_prop
+        uIpdf_final[i] += poisson.pmf(e, u_rate*0.1) * lower_prop
+        uIpdf_final[i+1] += poisson.pmf(e, u_rate*0.1) * upper_prop
+uIpdf = uIpdf_final
+
+uI_res = int((uI_max-uI_min) / ((u_max-u_min)/u_res))+1
+pymiind_uI = [0 for a in range(uI_res)]
+ratio = I_res / uI_res
+val_counter = 0.0
+pos_counter = 0
+for i in range(I_res):
+    pos_counter += 1
+    if pos_counter > ratio:
+        pos_counter = 0
+        val_counter += uIpdf[i] * (i % ratio)
+        pymiind_uI[int(i/ratio)] = val_counter
+        val_counter = uIpdf[i] * (1.0-(i % ratio))
+    else:
+        val_counter += uIpdf[i]
+
+# Initialise the monte carlo neurons
+mc_neurons = np.array([[norm.rvs(-70.6, 0.1, 1)[0],norm.rvs(0.0, 0.1, 1)[0],norm.rvs(0.0, 0.1, 1)[0]] for a in range(5000)])
+
+dims = 3
+cell_widths = [(v_max-v_min)/v_res, (w_max-w_min)/w_res, (u_max-u_min)/u_res]
+
+initial_dist = np.ndarray((v_res, w_res, u_res))
+for cv in range(v_res):
+    for cw in range(w_res):
+        for cu in range(u_res):
+            initial_dist[cv,cw,cu] = vpdf[cv]*wpdf[cw]*updf[cu]
+
+solver = Solver(cond, initial_dist, np.array([v_min,w_min,u_min]), cell_widths, 0.00000001)
+solver.addNoiseKernel(pymiind_wI, 1)
+solver.addNoiseKernel(pymiind_uI, 2)
+
+for iteration in range(1000):
+
+    # Solver
+    solver.updateDeterministic()
+    solver.applyNoiseKernels()
+
+    # Also run the monte carlo simulation 
+    fired_count = 0
+    for nn in range(len(mc_neurons)):
+        mc_neurons[nn] = cond(mc_neurons[nn])
+
+        if (mc_neurons[nn][0] > -50.0):
+            mc_neurons[nn][0] = -70.6
+            fired_count+=1
+                
+        mc_neurons[nn][1] += epsp*poisson.rvs(w_rate*0.1) # override w
+        mc_neurons[nn][2] += ipsp*poisson.rvs(u_rate*0.1) # override u
+        
+
+    if (iteration % 50 == 0) :
+        fig, ax = plt.subplots(2,2)
+        ax[0,0].hist(mc_neurons[:,0], density=True, bins=v_res, range=[v_min,v_max], histtype='step')
+        ax[0,1].hist(mc_neurons[:,1], density=True, bins=w_res, range=[w_min,w_max], histtype='step')
+        ax[1,0].hist(mc_neurons[:,2], density=True, bins=u_res, range=[u_min,u_max], histtype='step')
+        
+        mpos, marginals = solver.calcMarginals()
+
+        marginals[0] = [a / (cell_widths[0]) for a in marginals[0]]
+        marginals[1] = [a / (cell_widths[1]) for a in marginals[1]]
+        marginals[2] = [a / (cell_widths[2]) for a in marginals[2]]
+
+        ax[0,0].scatter(mpos[0], marginals[0])
+        ax[0,1].scatter(mpos[1], marginals[1])
+        ax[1,0].scatter(mpos[2], marginals[2])
+
+        fig.tight_layout()
+        plt.show()
